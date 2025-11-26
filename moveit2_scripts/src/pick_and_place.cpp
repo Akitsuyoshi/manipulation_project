a0bfc75fd4a1ee2ca6881f6d2c44fb4dcb41904f
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-11-26 12:47:51 +0000

    Add move group interface for robot

--- moveit2_scripts/src/pick_and_place.cpp
+++ moveit2_scripts/src/pick_and_place.cpp
@@ -1,7 +1,3 @@
-#include "rclcpp/logger.hpp"
-#include "rclcpp/logging.hpp"
-#include "rclcpp/node.hpp"
-#include "rclcpp/node_options.hpp"
 #include <moveit/move_group_interface/move_group_interface.h>
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <moveit_msgs/msg/display_robot_state.hpp>
@@ -12,6 +8,7 @@
 #include <memory>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp_action/rclcpp_action.hpp>
+#include <string>
 #include <thread>
 #include <vector>
 
@@ -25,8 +22,55 @@ public:
 
     rclcpp::NodeOptions node_options;
     node_options.automatically_declare_parameters_from_overrides(true);
+    move_group_node_ =
+        rclcpp::Node::make_shared("move_group_node", node_options);
+    executor_.add_node(move_group_node_);
+    std::thread([this]() { this->executor_.spin(); }).detach();
+
+    // Init move group interfaces
+    move_group_robot_ = std::make_shared<MoveGroupInterface>(
+        move_group_node_, PLANNING_GROUP_ROBOT);
+    // Get init state of robot
+    joint_model_group_robot_ =
+        move_group_robot_->getCurrentState()->getJointModelGroup(
+            PLANNING_GROUP_ROBOT);
+    // print out basic system information
+    RCLCPP_INFO(LOGGER, "Planning Frame: %s",
+                move_group_robot_->getPlanningFrame().c_str());
+    RCLCPP_INFO(LOGGER, "End Effector Link: %s",
+                move_group_robot_->getEndEffectorLink().c_str());
+    RCLCPP_INFO(LOGGER, "Available Planning Groups:");
+    std::vector<std::string> group_names =
+        move_group_robot_->getJointModelGroupNames();
+    for (long unsigned int i = 0; i < group_names.size(); i++) {
+      RCLCPP_INFO(LOGGER, "Group %ld: %s", i, group_names[i].c_str());
+    }
+
+    // get current state of robot
+    current_state_robot_ = move_group_robot_->getCurrentState(10);
+    current_state_robot_->copyJointGroupPositions(joint_model_group_robot_,
+                                                  joint_group_positions_robot_);
+    // Set start state of robot to current state
+    move_group_robot_->setStartStateToCurrentState();
 
     RCLCPP_INFO(LOGGER, "Initialized Class: Pick And Place");
+  }
+
+  ~PickAndPlace() { RCLCPP_INFO(LOGGER, "Class Terminated"); }
+
+  void execute_trajectory() {
+    RCLCPP_INFO(LOGGER, "Executing Pick And Place");
+
+    RCLCPP_INFO(LOGGER, "Preparing Joint Value Trajectory");
+    // setup_joint_value_target(+0.0000, -2.3562, +1.5708, -1.5708, -1.5708,
+    //                          +0.0000);
+    setup_goal_pose_target(+0.343, +0.132, +0.284, -1.000, +0.000, +0.000,
+                           +0.000);
+    RCLCPP_INFO(LOGGER, "Planning Joint Value Trajectory");
+    plan_trajectory_kinematics();
+    execute_trajectory_kinematics();
+
+    RCLCPP_INFO(LOGGER, "Complate");
   }
 
 private:
@@ -37,6 +81,56 @@ private:
   using Pose = geometry_msgs::msg::Pose;
 
   rclcpp::Node::SharedPtr base_node_;
+  rclcpp::Node::SharedPtr move_group_node_;
+  rclcpp::executors::SingleThreadedExecutor executor_;
+  std::shared_ptr<MoveGroupInterface> move_group_robot_;
+  const JointModelGroup *joint_model_group_robot_;
+
+  // trajectory planning variables for robot
+  std::vector<double> joint_group_positions_robot_;
+  RobotStatePtr current_state_robot_;
+  Plan kinematics_trajectory_plan_;
+  Pose target_pose_robot_;
+  bool plan_success_robot_ = false;
+
+  void setup_joint_value_target(float angle0, float angle1, float angle2,
+                                float angle3, float angle4, float angle5) {
+    joint_group_positions_robot_[0] = angle0; // Shoulder Pan
+    joint_group_positions_robot_[1] = angle1; // Shoulder Lift
+    joint_group_positions_robot_[2] = angle2; // Elbow
+    joint_group_positions_robot_[3] = angle3; // Wrist 1
+    joint_group_positions_robot_[4] = angle4; // Wrist 2
+    joint_group_positions_robot_[5] = angle5; // Wrist 3
+    move_group_robot_->setJointValueTarget(joint_group_positions_robot_);
+  }
+
+  void setup_goal_pose_target(float pos_x, float pos_y, float pos_z,
+                              float quat_x, float quat_y, float quat_z,
+                              float quat_w) {
+    target_pose_robot_.position.x = pos_x;
+    target_pose_robot_.position.y = pos_y;
+    target_pose_robot_.position.z = pos_z;
+    target_pose_robot_.orientation.x = quat_x;
+    target_pose_robot_.orientation.y = quat_y;
+    target_pose_robot_.orientation.z = quat_z;
+    target_pose_robot_.orientation.w = quat_w;
+    move_group_robot_->setPoseTarget(target_pose_robot_);
+  }
+
+  void plan_trajectory_kinematics() {
+    plan_success_robot_ =
+        (move_group_robot_->plan(kinematics_trajectory_plan_) ==
+         moveit::core::MoveItErrorCode::SUCCESS);
+  }
+
+  void execute_trajectory_kinematics() {
+    if (plan_success_robot_) {
+      move_group_robot_->execute(kinematics_trajectory_plan_);
+      RCLCPP_INFO(LOGGER, "Robot Kinematics Trajectory Success!");
+    } else {
+      RCLCPP_INFO(LOGGER, "Robot Kinematics Trajectory Failed!");
+    }
+  }
 };
 
 int main(int argc, char **argv) {
@@ -45,6 +139,7 @@ int main(int argc, char **argv) {
   std::shared_ptr<rclcpp::Node> base_node =
       std::make_shared<rclcpp::Node>("pick_and_place");
   PickAndPlace pick_and_place(base_node);
+  pick_and_place.execute_trajectory();
 
   rclcpp::shutdown();
   return 0;
