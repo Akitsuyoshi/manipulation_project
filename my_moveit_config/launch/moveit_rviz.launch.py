d803cc734404d79965d23c004f56b51f0162ba14
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-11-26 08:43:06 +0000

    Add a new moveit config files for sim

---
+++ my_moveit_config/launch/moveit_rviz.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from moveit_configs_utils import MoveItConfigsBuilder
+
+def generate_launch_description():
+
+    moveit_config = MoveItConfigsBuilder("name",package_name="my_moveit_config").to_moveit_configs()
+
+    # RViz Node
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="moveit_rviz",
+        output="screen",
+        parameters=[
+            moveit_config.to_dict(),
+            {"use_sim_time": True},
+        ],
+        arguments=[
+            "-d", str(moveit_config.package_path / "config" / "moveit.rviz"),
+        ],
+    )
+
+    return LaunchDescription([rviz_node])
