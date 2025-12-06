fe59b1c0f9c5993978d790c4ca8893850a0c56ff
Tsuyoshi Akiyama <ka090824@gmail.com>
2025-12-06 01:31:54 +0000

    Add a custom interfaces pkg

--- object_detection/object_detection/object_detection.py
+++ object_detection/object_detection/object_detection.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import rclpy
 from rclpy.node import Node
+from custom_interfaces.msg import DetectedSurfaces, DetectedObjects
 
 class ObjectDetection(Node):
     def __init__(self) -> None:
